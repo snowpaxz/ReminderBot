a47ba32ed8ec0f2952e00447258364a74db1dde6
Lars <ltlofquist@gmail.com>
2022-03-06 10:53:16 -0600

    ! and !p works in sr design channel, sends on time

--- bot.py
+++ bot.py
@@ -1,21 +1,45 @@
 """ A simple Discord bot made to reply to exclamation points.
     TODO: Add time based reminders to different events, like due dates """
 import os
+from datetime import datetime, time, timedelta
+import asyncio
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.utils import get
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = int(os.getenv('DISCORD_GUILD'))
+CHANNEL = int(os.getenv('DISCORD_CHANNEL'))
 
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix= '.', intents=intents)
+WHEN = datetime(2022, 3, 4, 22, 0, 0) #10:00.00pm
+print(f'Original WHEN: {WHEN}')
+
+async def turnin_reminder():
+    """ Fires the weekly reminder in defined channel """
+    await bot.wait_until_ready()
+    channel = bot.get_guild(GUILD).get_channel(CHANNEL)
+    await channel.send("Do your weekly review!")
+
+@tasks.loop(minutes=30)
+async def background_keeper():
+    """ Station keeping function, checks if time to fire  """
+    global WHEN
+
+    now = datetime.now()
+    FRIDAY = 4
+    if now >= WHEN:
+        if now.weekday() == FRIDAY:
+            print("Valid Weekday")
+            await turnin_reminder()
+        WHEN =  WHEN + timedelta(days=7)
+        print(f'New WHEN: {WHEN}')
 
 @bot.event
 async def on_ready():
@@ -24,6 +48,7 @@ async def on_ready():
     print("With the ID: " + str(bot.user.id))
     print('Bot is ready to be used')
 
+    background_keeper.start()
     for guild in bot.guilds:
         print(f'Guild name: {guild}\nGuild ID: {guild.id}\n')
         print('Members:')
@@ -42,5 +67,6 @@ async def on_message(message):
             await message.channel.send('!pong')
         else:
             await message.channel.send('!')
+
 
 bot.run(TOKEN)
